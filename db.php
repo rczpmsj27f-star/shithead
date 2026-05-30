2e3d626439c418494b364b53aba79ff2ab4059d4
IanDC <rczpmsj27f@privaterelay.appleid.com>
2026-05-30 08:05:37 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -1,12 +1,14 @@
 <?php
-$host = "localhost";
-$dbname = "your_database_name";
-$user = "your_db_user";
-$pass = "your_db_password";
+
+$host = "YOUR_HOSTNAME_HERE";
+$dbname = "YOUR_DATABASE_NAME";
+$user = "YOUR_DATABASE_USER";
+$pass = "YOUR_DATABASE_PASSWORD";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (Exception $e) {
-    die("Database connection failed");
+    die("DB ERROR: " . $e->getMessage());
 }
