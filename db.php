0035086819412f7f51cc2a93e861a6788c6a7108
IanDC <rczpmsj27f@privaterelay.appleid.com>
2026-05-30 08:18:26 +0100

    Change database credentials in db.php
    Updated database connection credentials.

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "YOUR_HOSTNAME_HERE";
-$dbname = "YOUR_DATABASE_NAME";
-$user = "YOUR_DATABASE_USER";
-$pass = "YOUR_DATABASE_PASSWORD";
+$dbname = "u983097270_shithead";
+$user = "u983097270_shithead";
+$pass = "Farrell0405!!";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
